b53cbbf3b96af4001e6ccb9cd7be29c88f6e39cb
makersmarx <bbiehn1@gmail.com>
2020-12-10 16:23:12 -0600

    add generic footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,17 @@ import React from 'react';
 const Footer = () => {
   return (
     <div>
-      <p>Footer</p>
+      <ul>
+        <a href="https://github.com/Makersmarx?tab=repositories">
+          <li>Contact</li>
+        </a>
+        <a href="https://www.webhamster.com/">
+          <li>Time Warp</li>
+        </a>
+        <a href="https://www.youtube.com/watch?v=FIW915Su08o">
+          <li>Eject</li>
+        </a>
+      </ul>
     </div>
   );
 };
